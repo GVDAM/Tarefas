5495fe434e34d8d31814d521559b27d8c4a19684
GVDAM <gabrielvargasdam.19@gmail.com>
2024-10-14 16:44:56 -0300

    First commit

---
+++ pages/script.js
@@ -0,0 +1,11 @@
+function menu() {
+    window.location.href = "file:///C:/Users/Gabriel%20e%20Brenda/Documents/P%C3%81GINA%20VIVAFIT%20modelo/index.html";
+}
+
+function page_name() {
+    window.location.href = "file:///C:/Users/Gabriel%20e%20Brenda/Documents/P%C3%81GINA%20VIVAFIT%20modelo/pages/nome.html";
+}
+
+function page_task() {
+    window.location.href = "file:///C:/Users/Gabriel%20e%20Brenda/Documents/P%C3%81GINA%20VIVAFIT%20modelo/pages/tarefa.html";
+}
